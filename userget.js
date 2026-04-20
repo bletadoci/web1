f5e6989b52f7e9e127b1d56a51d70e044996f77a
bletadoci <bleta.doci@student.uni-pr.edu>
2026-04-20 18:09:38 +0200

    changes

--- userget.js
+++ userget.js
@@ -1,13 +1,14 @@
 //ver1
-//html part:
-<html>
-<input type="text" id="userInput" placeholder="Type something...">
-<button onclick="getValue()">Submit</button>
-</html>
 let age = prompt("How old are you?");
 let nextYear = Number(age) + 1; // Convert string to number
 
 //ver2
+//html part:
+<html>
+    <script src="script.js" defer></script>
+<input type="text" id="userInput" placeholder="Type something...">
+<button onclick="getValue()">Submit</button>
+</html>
 function getValue() {
     // 1. Find the element
     let inputField = document.getElementById("userInput");
@@ -17,3 +18,12 @@ function getValue() {
     
     alert("You typed: " + data);
 }
+
+let input = prompt("Enter names separated by commas (e.g. Ben, Ana, Leo):");
+
+// .split(",") takes the string and cuts it everywhere it sees a comma
+let namesArray = input.split(","); 
+
+console.log(namesArray); // Output: ["Ben", " Ana", " Leo"]
+/*Pro Tip: Users often add accidental spaces. You can clean them up using map and trim:
+let cleanArray = input.split(",").map(item => item.trim());*/
